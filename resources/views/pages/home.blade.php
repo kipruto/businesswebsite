8f6405b9e994ac6359d65d73ce2a6208c335f53a
Kipruto <ropenock@gmail.com>
2022-06-25 11:42:11 +0300

    first_push

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang= "{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <style>
+nav{
+    height:100;
+    background-color: #C7F0FE;
+
+}
+ul{
+    display: inline;
+
+}
+li{
+    list-style: none;
+}
+body{
+    background-color: #FBFCFD;
+    height: 90vh;
+
+}
+.sign-up{
+    
+}
+
+
+        </style>
+</head>
+<nav>
+    <div class='right-side-nav'>
+    <ul>
+        <li> <a>Home</a></li>
+        <li> <a>Services</a></li>
+        <li> <a>Blog</a></li>
+        <li> <a>Contact us</a></li>
+        <li> <a>Support</a></li>
+</ul>
+    </div>
+    <div class='left-side-nav'>
+    <ul>
+        <li> <a class='login-btn'>Login</a></li>
+        <li> <a class='sign-up'>Sign Up</a></li>
+</ul>
+</div>
+</nav>
+<body>
+
+    
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function(){
+    return view('home');
+});
+
+Route::get('/contacts', function(){
+    return view('contacts');
+});
+
+Route::get('/about', function(){
+    return view('about');
+});
+
+Route::get('/posts/{id}', function($id){
+    return view("posts/{$id}");
+});
+
+Route::post('/post', function(){
+      return;
+});
