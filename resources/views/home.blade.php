a3579c71cf085238f25649a20e2769edaea2392b
Kipruto <ropenock@gmail.com>
2022-06-26 18:56:25 +0300

    updated views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,32 +7,57 @@
     <title>Home</title>
 
     <style>
-nav{
-    height:100;
-    background-color: #C7F0FE;
 
-}
-ul{
-    display: inline;
-
-}
-li{
-    list-style: none;
-}
 body{
     background-color: #FBFCFD;
     height: 90vh;
-
+    width: 70vw;
+    margin: 0 auto;
 }
 .sign-up{
     
 }
+nav{
+    display:flex;
+    align-items:center;
+    height:100px;
+    width:100%;
+    background-color: #C7F0FE;
 
+}
+.left-side-nav{
+    float:left;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    width:60%;
+    background-color: #FBFCFD;
 
+}
+.right-side-nav{
+    float:right;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    width:40%;
+    background-color: #FBFCFD;
+
+}
+ul{
+    display: inline-flex;
+}
+li{
+    list-style: none;
+    margin: 5px 10px;
+    /* border: 1px solid black; */
+    padding: 5px 20px
+}
         </style>
 </head>
+
+<body>
 <nav>
-    <div class='right-side-nav'>
+    <div class='left-side-nav'>
     <ul>
         <li> <a>Home</a></li>
         <li> <a>Services</a></li>
@@ -41,15 +66,13 @@ body{
         <li> <a>Support</a></li>
 </ul>
     </div>
-    <div class='left-side-nav'>
+    <div class='right-side-nav'>
     <ul>
         <li> <a class='login-btn'>Login</a></li>
         <li> <a class='sign-up'>Sign Up</a></li>
 </ul>
 </div>
 </nav>
-<body>
-
     
 </body>
 </html>
